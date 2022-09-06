ed939308638f0c17b50c62d276924909d76d115a
evanchooly <jlee@antwerkz.com>
2022-09-06 17:56:56 -0400

    clean up

--- plugin/src/main/java/com/antwerkz/codegen/roaster/Roaster.java
+++ plugin/src/main/java/com/antwerkz/codegen/roaster/Roaster.java
@@ -1,27 +1,21 @@
 package com.antwerkz.codegen.roaster;
 
 import com.antwerkz.codegen.maven.Generator;
-import jdk.jfr.ContentType;
-import jdk.jfr.Enabled;
-import jdk.jfr.Name;
 import org.apache.maven.project.MavenProject;
-import org.jboss.forge.roaster.model.source.AnnotationElementSource;
 import org.jboss.forge.roaster.model.source.JavaClassSource;
 import org.jboss.forge.roaster.model.source.JavaDocSource;
-import org.jboss.forge.roaster.model.source.MethodSource;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.annotation.Documented;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Objects;
-import java.util.StringJoiner;
 
 import static java.lang.String.format;
-import static java.util.Arrays.asList;
 import static org.jboss.forge.roaster.Roaster.create;
+
+;
+;
 
 public class Roaster implements Generator {
     private JavaClassSource builder;
@@ -49,9 +43,8 @@ public class Roaster implements Generator {
                       .setPackage("com.antwerkz.generated");
 
         builder.addImport(Objects.class);
-        builder.addImport(Documented.class);
 
-        builder.addAnnotation(Enabled.class);
+        builder.addAnnotation("jdk.jfr.Enabled");
         builder.addAnnotation("SuppressWarnings")  // can also use String name
             .setStringArrayValue("value", new String[]{"all of it"});
 
@@ -66,7 +59,7 @@ public class Roaster implements Generator {
                .setBody("""
                    System.out.println(greeting());
                    """)
-            .addAnnotation(Name.class)
+            .addAnnotation("jdk.jfr.Name")
             .setStringValue("value", "Inigo Montoya");
 
         builder.addMethod()
